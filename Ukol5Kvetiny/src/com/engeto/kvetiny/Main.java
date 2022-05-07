f1c03ed63ebd889660dc9d662b791fcc17f9e9d2
Jakub Jiráček <jakub.jiracek99@gmail.com>
2022-05-07 11:48:52 +0200

    Přidání scanneru

--- Ukol5Kvetiny/src/com/engeto/kvetiny/Main.java
+++ Ukol5Kvetiny/src/com/engeto/kvetiny/Main.java
@@ -1,8 +1,22 @@
 package com.engeto.kvetiny;
 
+import java.io.FileNotFoundException;
+
 public class Main {
 
+
+    public static final String FILENAME = "kvetiny.txt";
+    public static final String DELIMITER = "\t";
+
     public static void main(String[] args) {
-	// write your code here
+
+
+        try {
+            System.out.println(PlantList.importFromFile(FILENAME, DELIMITER));
+        } catch (PlantException e) {
+            e.printStackTrace();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
--- Ukol5Kvetiny/src/com/engeto/kvetiny/PlantList.java
+++ Ukol5Kvetiny/src/com/engeto/kvetiny/PlantList.java
@@ -2,7 +2,10 @@ package com.engeto.kvetiny;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -10,6 +13,42 @@ import java.util.Scanner;
 public class PlantList {
 
     List<Plant> plants = new ArrayList<>();
+
+    public static String importFromFile(String filename, String delimiter)
+            throws PlantException, FileNotFoundException {
+        String nextLine = "";
+        String[] items = new String[1];
+        String name;
+        String notes;
+        BigDecimal frequencyOfWatering;
+        LocalDate planted;
+        LocalDate watering;
+        int lineNumber = 0;
+
+        PlantList result = new PlantList();
+
+        try (Scanner scanner = new Scanner(new BufferedReader(new FileReader(filename)))) {
+            while (scanner.hasNextLine()) {
+                lineNumber++;
+                nextLine = scanner.nextLine();
+                items = nextLine.split("\t");
+                name = items[0];
+                notes = items[1];
+                frequencyOfWatering = new BigDecimal(items[2]);
+                planted = LocalDate.parse(items[3]);
+                watering = LocalDate.parse(items[4]);
+                result.addPlant(new Plant(name,notes,frequencyOfWatering,planted,watering));
+            }
+            }
+        catch (FileNotFoundException e) {
+        try {
+            throw new PlantException("Soubor nebyl nalezen");
+        } catch (PlantException ex) {
+            ex.printStackTrace();
+        }
+    }
+        return result;
+    }
 
     public void addPlant(Plant plant) {
         plants.add(plant);
@@ -22,7 +61,5 @@ public class PlantList {
     public void removePlant(int index) {
         plants.remove(index);
     }
-
-
 
 }
